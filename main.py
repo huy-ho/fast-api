09df07f793d8c4c22c236cfc191e1e713a08f5a5
Huy Ho <67500193+huy-ho@users.noreply.github.com>
2023-02-14 15:27:45 -0800

    creating post requests

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 
-#route / path operations
+# route / path operations
 
-@app.get("/") #this is a decorator to reference the fastapi. This is a get request
+@app.get("/")  # this is a decorator to reference the fastapi. This is a get request
 async def root():
-    return {"message": "interesting"}
+    return {"message": "hello world"}
+
+
+# when you are retrieving data, it is usually a get operations
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
+
+@app.post("/createposts")
+def create_posts(payLoad: dict = Body(...)):
+    print(payLoad)
+    return {"new_post": f"title: {payLoad['title']} content: {payLoad['content']}"}
